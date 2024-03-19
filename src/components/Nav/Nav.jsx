85aac25ab3e653cf216fef5621803cb5ebf4637e
Ariel Rodriguez <r.ariel94@gmail.com>
2024-03-19 18:30:09 -0500

    log out functionality added to sidebar, removed log out from top navigation

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import "./Nav.css";
+import { useSelector } from "react-redux";
+import { useDispatch } from 'react-redux';
 
-function Nav() {
+// MUI for Sidebar Testing
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import CalendarMonthRoundedIcon from "@mui/icons-material/CalendarMonthRounded";
+import GroupsIcon from "@mui/icons-material/Groups";
+import ManageAccountsRoundedIcon from "@mui/icons-material/ManageAccountsRounded";
+import ExitToAppRoundedIcon from "@mui/icons-material/ExitToAppRounded";
+
+const drawerWidth = 240;
+
+function Nav(props) {
   const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
 
   return (
     <div className="nav">
@@ -24,15 +49,81 @@ function Nav() {
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
+            <Box sx={{ display: "flex" }}>
+              <CssBaseline />
+              <AppBar
+                position="fixed"
+                sx={{
+                  width: `calc(100% - ${drawerWidth}px)`,
+                  ml: `${drawerWidth}px`,
+                }}
+              ></AppBar>
+              <Drawer
+                sx={{
+                  width: drawerWidth,
+                  flexShrink: 0,
+                  "& .MuiDrawer-paper": {
+                    width: drawerWidth,
+                    boxSizing: "border-box",
+                  },
+                }}
+                variant="permanent"
+                anchor="left"
+              >
+                <Toolbar />
+                <div className="profileIcon">
+                  {<AccountCircleRoundedIcon fontSize="large" />}
+                </div>
+                <Divider />
+                <List>
+                  {[
+                    "Home",
+                    "Schedule",
+                    "Meet The Tutors",
+                    "Update Profile",
+                  ].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                      <ListItemButton>
+                        <ListItemIcon>
+                          {index === 0 && <HomeRoundedIcon fontSize="medium" />}
+                          {index === 1 && (
+                            <CalendarMonthRoundedIcon fontSize="medium" />
+                          )}
+                          {index === 2 && <GroupsIcon fontSize="medium" />}
+                          {index === 3 && (
+                            <ManageAccountsRoundedIcon fontSize="medium" />
+                          )}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </List>
+                <Divider />
+                <div className="logOutSidebar">
+                  <List >
+                    {['Log Out'].map((text, index) => (
+                      <ListItem key={text} disablePadding>
+                        <ListItemButton onClick={() => dispatch({ type: 'LOGOUT' })}>
+                          <ListItemIcon>
+                            {<ExitToAppRoundedIcon fontSize="medium" />}
+                          </ListItemIcon>
+                          <ListItemText primary={text} />
+                        </ListItemButton>
+                      </ListItem>
+                    ))}
+                  </List>
+                </div>
+              </Drawer>
+            </Box>
             <Link className="navLink" to="/user">
               Home
             </Link>
 
             <Link className="navLink" to="/info">
               Info Page
-            </Link>
+            </Link>   
 
-            <LogOutButton className="navLink" />
           </>
         )}
 
@@ -45,3 +136,8 @@ function Nav() {
 }
 
 export default Nav;
+
+
+
+
+ 
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,27 +1,6 @@
 import * as React from "react";
 import LogOutButton from "../LogOutButton/LogOutButton";
 import { useSelector } from "react-redux";
-// MUI for Sidebar Testing
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import CssBaseline from "@mui/material/CssBaseline";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
-import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
-import CalendarMonthRoundedIcon from "@mui/icons-material/CalendarMonthRounded";
-import GroupsIcon from "@mui/icons-material/Groups";
-import ManageAccountsRoundedIcon from "@mui/icons-material/ManageAccountsRounded";
-import ExitToAppRoundedIcon from "@mui/icons-material/ExitToAppRounded";
-
-const drawerWidth = 240;
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -29,75 +8,7 @@ function UserPage() {
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          sx={{
-            width: `calc(100% - ${drawerWidth}px)`,
-            ml: `${drawerWidth}px`,
-          }}
-        ></AppBar>
-        <Drawer
-          sx={{
-            width: drawerWidth,
-            flexShrink: 0,
-            "& .MuiDrawer-paper": {
-              width: drawerWidth,
-              boxSizing: "border-box",
-            },
-          }}
-          variant="permanent"
-          anchor="left"
-        >
-          <Toolbar />
-          <div className="profileIcon">
-            {<AccountCircleRoundedIcon fontSize="large" />}
-          </div>
-          <Divider />
-          <List>
-            {["Home", "Schedule", "Meet The Tutors", "Update Profile"].map(
-              (text, index) => (
-                <ListItem key={text} disablePadding>
-                  <ListItemButton>
-                    <ListItemIcon >
-                      {index === 0 && <HomeRoundedIcon fontSize="medium" />}
-                      {index === 1 && <CalendarMonthRoundedIcon fontSize="medium" />}
-                      {index === 2 && <GroupsIcon fontSize="medium" />}
-                      {index === 3 && <ManageAccountsRoundedIcon fontSize="medium" />}
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItemButton>
-                </ListItem>
-              )
-            )}
-          </List>
-          <Divider />
-          <div className="logOutSidebar">
-          <List>
-            {["Log Out"].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {<ExitToAppRoundedIcon fontSize="medium" />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          </div>
-        </Drawer>
-        <Box
-          component="main"
-          sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
-        >
-          <Toolbar />
-          <Typography paragraph>
-           
-          </Typography>
-        </Box>
-      </Box>
+      
       <p>Your ID is: {user.id}</p>
       <LogOutButton className="btn" />
     </div>
