893b761cff51e79f8c88fd92e93446ff195466c9
Ariel Rodriguez <r.ariel94@gmail.com>
2024-04-09 00:32:48 -0500

    found bug on calendar, setstartdate has to be clicked twice before triggering calendar for specific times

--- src/components/TutorsPage/TutorsPage.jsx
+++ src/components/TutorsPage/TutorsPage.jsx
@@ -5,6 +5,27 @@ import { useHistory } from "react-router-dom";
 // MUI
 import { Button } from "@mui/material";
 import TutorDetails from "../TutorDetails/TutorDetails";
+import {
+  createTheme,
+  alpha,
+  getContrastRatio,
+  ThemeProvider,
+} from "@mui/material/styles";
+
+const violetBase = "#7F00FF";
+const violetMain = alpha(violetBase, 0.7);
+
+const theme = createTheme({
+  palette: {
+    violet: {
+      main: violetMain,
+      light: alpha(violetBase, 0.5),
+      dark: alpha(violetBase, 0.9),
+      contrastText:
+        getContrastRatio(violetMain, "#fff") > 4.5 ? "#fff" : "#111",
+    },
+  },
+});
 
 function TutorsPage() {
   const dispatch = useDispatch();
@@ -20,26 +41,34 @@ function TutorsPage() {
   };
 
   return (
-    <div className="updateFormPanel">
-      <div>
-        <h2 >
-          <u>Meet The Tutors</u>
-        </h2>
+    <ThemeProvider theme={theme}>
+      <div className="updateFormPanel">
+        <div>
+          <h2>
+            <u>Meet The Tutors</u>
+          </h2>
+        </div>
+        <section className="tutors">
+          {tutors &&
+            tutors.map((tutor) => {
+              return (
+                <div className="tutorItem" key={tutor.id}>
+                  <h1 sx={{ "& button": { m: 1 } }}>
+                    <Button
+                      size="large"
+                      variant="contained"
+                      color="violet"
+                      onClick={clickTutorDetailsHandler(tutor.id)}
+                    >
+                      {tutor.full_name}
+                    </Button>
+                  </h1>
+                </div>
+              );
+            })}
+        </section>
       </div>
-      <section className="tutors">
-        {tutors &&
-          tutors.map((tutor) => {
-            return (
-              <div data-testid="tutorItem" key={tutor.id}>
-                <h1 sx={{ '& button': { m: 2 } }}>
-                <Button size="large" variant="outlined" onClick={clickTutorDetailsHandler(tutor.id)} >
-                  {tutor.full_name} 
-                </Button></h1>
-              </div> 
-            );
-          })}
-      </section>
-    </div>
+    </ThemeProvider>
   );
 }
 
